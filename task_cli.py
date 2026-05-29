2145c71c694d06722c900f44007de499e78ed489
paghiya <j1.pariya@gmail.com>
2026-05-29 06:49:17 +0330

    Update task_cli with fixes

--- task_cli.py
+++ task_cli.py
@@ -21,12 +21,13 @@ def save_tasks(tasks):
 
 def add_task(description):
     tasks = load_tasks()
+    new_id = max((t["id"] for t in tasks), default=0) + 1
     
 
-    if len(tasks) == 0:
-        new_id = 1
-    else:
-        new_id = tasks[-1]["id"] + 1
+    #if len(tasks) == 0: # Not efficient if the last task on the list is not the task with the last id as well
+        #new_id = 1      # Like if you've removed id 3 (and you got 4 ids at the first place) by manual removal from .json
+    #else:
+        #new_id = tasks[-1]["id"] + 1
     
 
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -58,10 +59,45 @@ def list_tasks(filter_status=None):
     for task in tasks:
         print(f"[{task['id']}] {task['description']} - {task['status']}")
 
+def delete_task(task_id):
+    tasks = load_tasks()
+    
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            save_tasks(tasks)
+            print(f"Task {task_id} deleted successfully")
+            return
+    
+    print(f"Error: Task {task_id} not found")
+
+def update_task(task_id, new_description):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            task["description"] = new_description
+            task["updatedAt"] = now
+            save_tasks(tasks)
+            print("Task updated successfully!")
+            return
+    print("Task not found!")
+    
+def mark_task(task_id, new_status):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = new_status
+            task["updatedAt"] = datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} marked as {new_status}.")
+            return
+    print("Task not found!")    
+
 def main():
     if len (sys.argv) < 2:
         print("Usage: python task_cli.py <command>")
-        print("Commands: add, list, update, delete, mark-done, mark-in-process")
+        print("Commands: add, list, update, delete, mark-done, mark-in-progress")
         return
     
     command = sys.argv[1]
@@ -78,8 +114,40 @@ def main():
             list_tasks()
         else:
             list_tasks(sys.argv[2])
+            
+    elif command == "delete":
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task ID")
+        else:
+            delete_task(int(sys.argv[2]))
+            
+    elif command == "update":
+        if len(sys.argv) < 4:
+            print("Error: Please provide a task ID")
+        else:
+            update_task(int(sys.argv[2]), sys.argv[3])   
+    
+    elif command == "mark-done":
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task ID")
+        else:
+            mark_task(int(sys.argv[2]), "done")
+
+    elif command == "mark-in-progress":
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task ID")
+        else:
+            mark_task(int(sys.argv[2]), "in-progress")
+
+    elif command == "mark-todo":
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task ID")
+        else:
+            mark_task(int(sys.argv[2]), "todo")
+  
     else:
         print(f"Unknown command: {command}")
+        
 
 main() 
 
